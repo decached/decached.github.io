82794779b562df71f975295e1edd89bf4f24baf6
Akash Kothawale <akash@decached.com>
2018-02-12 23:10:42 -0500

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+#
+# deploy.sh
+# Copyright (C) 2018 Akash Kothawale <akothaw1@binghamton.edu>
+#
+# Distributed under terms of the MIT license.
+#
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
